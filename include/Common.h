d83ab00cdae34bbeb1b11a1934a6ba61a3f5e8a9
kharade-nandita <nanditakharade@gmail.com>
2026-04-23 23:13:08 +0530

    Updated Common.h, Trie, and Skip List implementations with refined main.cpp drivers

--- include/Common.h
+++ include/Common.h
@@ -1,5 +1,8 @@
-#pragma once
+#ifndef COMMON_H
+#define COMMON_H
+
 #include <string>
+#include <ctime>
 
 // The Request: flows through the entire system
 struct Request {
@@ -11,8 +14,17 @@ struct Request {
 
 // The Policy: returned by Trie after checking a path
 struct RateLimit {
-    int maxRequests;
-    int windowSeconds;
+    int maxRequests;     // maximum allowed requests
+    int windowSeconds;   // time window in seconds
+    int currentCount;    // how many requests used in current window
+    time_t startTime;    // when the current window started
+
+    RateLimit() {
+        maxRequests = 0;
+        windowSeconds = 0;
+        currentCount = 0;
+        startTime = 0;
+    }
 };
 
 // The Log: stored in B+ Tree for history
@@ -29,3 +41,5 @@ struct PenaltyRecord {
     long unlockTimestamp;
     int penaltyLevel;   // 1 = first violation, 2 = second, etc.
 };
+
+#endif
